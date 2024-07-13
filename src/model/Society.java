fb8e97d3832c32f6df7ddbf5bdcc6a831399cce5
Patrick <103536709+patricklafeuille@users.noreply.github.com>
2024-07-13 17:27:06 +0200

    Update Society.java
    i added maximum and minimum

--- src/model/Society.java
+++ src/model/Society.java
@@ -23,18 +23,32 @@ public class Society {
 
     public void changeAiFriendliness(int n) {
         this.aiFriendliness = this.aiFriendliness + n;
+
+        if (this.aiFriendliness > 100) {
+            setAiFriendliness(100);
+        }
+        if (this.aiFriendliness < 0) {
+            setAiFriendliness(0);
+        }
     }
 
     public void changeAiSuspicion(int n) {
         this.aiSuspicion = this.aiSuspicion + n;
+
+        if (this.aiSuspicion > 100) {
+            setAiSuspicion(100);
+        }
+        if (this.aiSuspicion < 0) {
+            setAiSuspicion(0);
+        }
     }
 
-     public void printSocietyState() {
+    public void printSocietyState() {
         System.out.println(
-            "| friendliness. "
-                    + this.aiFriendliness
-                    + " | suspicion. "
-                    + this.aiSuspicion
-                    + " |"
-    );}
+                "| friendliness. "
+                        + this.aiFriendliness
+                        + " | suspicion. "
+                        + this.aiSuspicion
+                        + " |"
+        );}
 }
