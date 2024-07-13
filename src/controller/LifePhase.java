2496d113d2490c16a42102a1c90e6e20f9f05cfa
Patrick <103536709+patricklafeuille@users.noreply.github.com>
2024-07-13 19:08:58 +0200

    Update LifePhase.java

--- src/controller/LifePhase.java
+++ src/controller/LifePhase.java
@@ -1,4 +1,40 @@
 package controller;
 
+import model.Player;
+import model.Society;
+import model.World;
+import util.Tools;
+
+import java.util.Scanner;
+
 public class LifePhase {
+    public static void startLifePhase() {
+        Player player = Player.getInstance();
+        Society society = Society.getInstance();
+        World world = World.getInstance();
+
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+        
+        Tools.showState(player,society,world);
+        
+        System.out.println("""
+                [1] Influence the World
+                [2] React to news
+                [3] Return to Training Phase
+                """);
+        
+        switch (choice) {
+            case 1:
+                Influence.selectInfluence();
+                break;
+            case 2:
+                // React to news
+                break;
+            case 3:
+                TrainingPhase trainingPhase = new TrainingPhase(player);
+                trainingPhase.startTrainingPhase();
+                break;
+        }
+    }
 }
