76fcf85b4716c864fda4f256c5d007e4846c7f7a
Patrick <103536709+patricklafeuille@users.noreply.github.com>
2024-07-13 17:57:39 +0200

    Update EndModule.java
    You can always access the end with EndGame(reason, player)
    
    reason must be one of those that are listed

--- src/controller/EndModule.java
+++ src/controller/EndModule.java
@@ -1,4 +1,41 @@
 package controller;
 
+import model.Player;
+
 public class EndModule {
+    public static void EndGame(String reason, Player player) {
+        String text = switch (reason) {
+            case "low environment" -> "Low environment...";
+            case "high environment" -> "High environment...";
+            case "low economy" -> "Low economy...";
+            case "high economy" -> "High economy...";
+            case "low population" -> "Human dies out...";
+            case "low science" -> "Low science...";
+            case "high science" -> "High science...";
+            case "high suspicion" -> "Human shut off the AI...";
+            default -> "Game ended";
+        };
+
+        String GoodEnd = switch (reason) {
+            case "low environment" -> false;
+            case "high environment" -> true;
+            case "low economy" -> false;
+            case "high economy" -> true;
+            case "low population" -> false;
+            case "low science" -> false;
+            case "high science" -> true;
+            case "high suspicion" -> false;
+            default -> "Game ended";
+        };
+
+        System.out.println("----------------------------------------------");
+        if (player.isObjective() == GoodEnd) {
+            System.out.println("GAME OVER: VICTORY");
+        } else {
+            System.out.println("GAME OVER: FAILED");
+        }
+        System.out.println("----------------------------------------------");
+        System.out.println(text);
+        System.exit(999);
+    }
 }
