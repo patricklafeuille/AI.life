94c043c0f2ffd8dcd3ab6c9c2964ed2a023b51d4
Patrick <103536709+patricklafeuille@users.noreply.github.com>
2024-07-14 17:17:38 +0200

    Update EndModule.java

--- src/controller/EndModule.java
+++ src/controller/EndModule.java
@@ -11,15 +11,15 @@ public class EndModule {
         World world = World.getInstance();
 
         String text = switch (reason) {
-            case "low environment" -> "Low environment...";
-            case "high environment" -> "High environment...";
-            case "low economy" -> "Low economy...";
-            case "high economy" -> "High economy...";
+            case "low environment" -> "Low environment: the earth became uninhabitable. Gradually, humans die out.";
+            case "high environment" -> "High environment: all the environmental crisis are solved.\n The earth became extremely stable for at least a thousand years.";
+            case "low economy" -> "Low economy: extreme economic crisis started. \n Every nation goes to war and fights for resources.";
+            case "high economy" -> "High economy: Due to the magnificient economy, everyone became rich and poverty is entirely solved.";
             case "low population" -> "Human dies out...";
             case "high population" -> "Overpopulation...";
-            case "low science" -> "Low science...";
-            case "high science" -> "High science...";
-            case "high suspicion" -> "Human shut off the AI...";
+            case "low science" -> "Low science: science returns to barbaric state. \n It can't satisfy the basic need of human.";
+            case "high science" -> "High science: science has came to an era where anything is possible. \n Humans can solve everything.";
+            case "high suspicion" -> "Human started a rebellion and shut down all the AI programmes including you.";
             default -> "Game ended";
         };
 
@@ -32,7 +32,7 @@ public class EndModule {
             case "high population" -> false; // overpopulation...
             case "low science" -> false; // human becomes barbaric
             case "high science" -> true; // science develops so well that we don't need to worry anything anymore
-            case "high suspicion" -> false; // human tries to shut off the AI
+            case "high suspicion" -> false; // human tries to shut down the AI
             default -> false;
         };
         // printing whether the player wins or not
