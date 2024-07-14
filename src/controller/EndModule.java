c36a8e020e0d541c32c7b2fc17b4f73a2279fa90
Patrick <103536709+patricklafeuille@users.noreply.github.com>
2024-07-14 16:59:09 +0200

    Update EndModule.java

--- src/controller/EndModule.java
+++ src/controller/EndModule.java
@@ -1,11 +1,14 @@
 package controller;
 
 import model.Player;
+import model.World;
+import util.Tools;
 
 public class EndModule {
     public static void EndGame(String reason) {
 
         Player player = Player.getInstance();
+        World world = World.getInstance();
 
         String text = switch (reason) {
             case "low environment" -> "Low environment...";
@@ -21,18 +24,18 @@ public class EndModule {
         };
 
         boolean GoodEnd = switch (reason) {
-            case "low environment" -> false;
-            case "high environment" -> true;
-            case "low economy" -> false;
-            case "high economy" -> true;
-            case "low population" -> false;
-            case "high population" -> false; // "Overpopulation...
-            case "low science" -> false;
-            case "high science" -> true;
-            case "high suspicion" -> false;
+            case "low environment" -> false; // the earth became uninhabitable
+            case "high environment" -> true; // the climate change is entire solved. The environment is very stable
+            case "low economy" -> false; // the entire economic system collapses
+            case "high economy" -> true; // human has enough resources and everyone is rich
+            case "low population" -> false; // human dies out
+            case "high population" -> false; // overpopulation...
+            case "low science" -> false; // human becomes barbaric
+            case "high science" -> true; // science develops so well that we don't need to worry anything anymore
+            case "high suspicion" -> false; // human tries to shut off the AI
             default -> false;
         };
-
+        // printing whether the player wins or not
         System.out.println("----------------------------------------------");
         if (reason.equals("high suspicion") || player.isObjective() != GoodEnd) {
             System.out.println("GAME OVER: FAILED");
@@ -40,7 +43,8 @@ public class EndModule {
             System.out.println("GAME OVER: VICTORY");
         }
         System.out.println("----------------------------------------------");
-        System.out.println(text);
-        System.exit(666);
+        System.out.println(text); // printing the reason of the game over.
+        Tools.showState(player, world);
+        System.exit(999); // END OF THE GAME
     }
 }
