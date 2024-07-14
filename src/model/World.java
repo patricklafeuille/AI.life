49588ef8ecbd4e290ec0503d3a88ba9a01794478
Patrick <103536709+patricklafeuille@users.noreply.github.com>
2024-07-14 17:11:02 +0200

    Update World.java

--- src/model/World.java
+++ src/model/World.java
@@ -4,14 +4,14 @@ import static controller.EndModule.EndGame;
 
 public class World {
 
-    private static int week;
-    private double population; //in billions, 0 - 12
-    private double human_growth_rate; // -10 - 10
-    private int environment; // 1-100
-    private int economy; // 1-100
-    private int science; //
-    private int aiSuspicion;
-    private static World instance;
+    private static int week; // week since the beginning of the game
+    private double population; //in billions, 0 to 12
+    private double human_growth_rate; // -10 to 10
+    private int environment; // 1 to 100
+    private int economy; // 1 to 100
+    private int science; // 1 to 100
+    private int aiSuspicion; // How much the society finds the AI suspicious, and how much they want to shut the AI down: 1 to 100
+    private static World instance; // making World an instance so it can be accessed more easily.
 
     // Method to calculate probabilities
     public double getPopulationProbability() {
@@ -102,61 +102,61 @@ public class World {
     public void changePopulation(double n) {
         this.population += n;
 
-        if (this.population < 0) {
+        if (this.population < 0) { // minimal population
             setPopulation(0);
-            EndGame("low population");
-        } else if (this.population > 12) {
+            EndGame("low population"); // triggering EndModule: GAME OVER
+        } else if (this.population > 12) { // maximal population
             setPopulation(12);
-            EndGame("high population");
+            EndGame("high population"); // triggering EndModule: GAME OVER
         }
     }
 
     public void changeEnvironment(int n) {
         this.environment = this.environment + n;
 
-        if (this.environment > 100) {
+        if (this.environment > 100) { // maximal environment
             setEnvironment(100);
-            EndGame("high environment");
+            EndGame("high environment"); // triggering EndModule: GAME OVER
         }
-        if (this.environment < 0) {
+        if (this.environment < 0) { // minimal environment
             setEnvironment(0);
-            EndGame("low environment");
+            EndGame("low environment"); // triggering EndModule: GAME OVER
         }
     }
 
     public void changeEconomy(int n) {
         this.economy = this.economy + n;
 
-        if (this.economy > 100) {
+        if (this.economy > 100) { // maximal economy
             setEconomy(100);
-            EndGame("high economy");
+            EndGame("high economy"); // triggering EndModule: GAME OVER
         }
-        if (this.economy < 0) {
+        if (this.economy < 0) { // minimal economy
             setEconomy(0);
-            EndGame("low economy");
+            EndGame("low economy"); // triggering EndModule: GAME OVER
         }
     }
 
     public void changeScience(int n) {
         this.science = this.science + n;
 
-        if (this.science > 100) {
+        if (this.science > 100) { // maximal science
             setScience(100);
-            EndGame("high science");
+            EndGame("high science"); // triggering EndModule: GAME OVER
         }
-        if (this.science < 0) {
+        if (this.science < 0) { // minimal science
             setScience(0);
-            EndGame("low science");
+            EndGame("low science"); // triggering EndModule: GAME OVER
         }
     }
 
     public void changeGrowth(double n) {
         this.human_growth_rate = this.human_growth_rate + n;
 
-        if (this.human_growth_rate > 10) {
+        if (this.human_growth_rate > 10) { // maximal human growth rate
             setGrowth(10);
         }
-        if (this.human_growth_rate < -10) {
+        if (this.human_growth_rate < -10) { // minimal human growth rate
             setGrowth(-10);
         }
     }
@@ -164,11 +164,11 @@ public class World {
     public void changeAiSuspicion(int n) {
         this.aiSuspicion = this.aiSuspicion + n;
 
-        if (this.aiSuspicion > 100) {
+        if (this.aiSuspicion > 100) { // maximal AI Suspicion
             setAiSuspicion(100);
-            EndGame("high suspicion");
+            EndGame("high suspicion"); // triggering EndModule: GAME OVER
         }
-        if (this.aiSuspicion < 0) {
+        if (this.aiSuspicion < 0) { // minimal AI Suspicion
             setAiSuspicion(0);
         }
     }
